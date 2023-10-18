b4907dfb20463e53b8e173dac4853aa4c930498a
Kevin Patto <kevp2010@gmail.com>
2023-10-18 14:44:41 -0500

    post-initial commits

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
