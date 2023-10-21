4b53f1ebd75c0b14379afd8bf59cb9c79e9eb713
Kevin Patto <kevp2010@gmail.com>
2023-10-21 16:29:52 -0500

    server edit fixed post, partially completed delete functionality

--- routes/notes.js
+++ routes/notes.js
@@ -1,6 +1,6 @@
 // server path /api/notes
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../utils/fsUtils.js'); // fs helpers (NOT MY CODE)
+const { readFromFile, writeToFile, readAndAppend } = require('../utils/fsUtils.js'); // fs helpers (NOT MY CODE)
 const { v4 } = require('uuid');
 
 notes.get('/', (req, res) => {
@@ -21,15 +21,29 @@ notes.post('/', (req, res) => {
 
         console.log(newNote);
 
-        // readAndAppend(newNote, './db/db.json');
+        readAndAppend(newNote, './db/db.json');
         res.json(`Note added successfully`);
     } else {
         res.error('Error in adding note');
     }
 });
 
-notes.delete('/', (req, res) => {
+notes.delete('/:id', (req, res) => {
+    res.error('this feature is not yet finished');
+    // const idDel = req.params.id;
+    // let notesList = [];
+    // readFromFile('./db/db.json')
+    //     .then((data) => {
+    //         notesList = JSON.parse(data)
+    //     })
+    //     .then(() => 
+    //         console.log(notesList)
+    //     );
+    // notesList.forEach(element => {
+    //     if (element.title === idDel) {
 
+    //     }
+    // });
 });
 
 module.exports = notes;
