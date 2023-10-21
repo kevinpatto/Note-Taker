ce9d43d66a1796a76bd3a4d0281c76e69bd09e8c
Kevin Patto <kevp2010@gmail.com>
2023-10-21 15:53:44 -0500

    routing done

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+// server path /
 const express = require('express');
+const path = require('path');
+const api = require('./routes/index.js')
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 app.use(express());
 app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static('public'));
-app.listen(PORT, () => console.log(`Listening on PORT: PORT`));
+app.use('/api', api); // route to /api
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')))
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
